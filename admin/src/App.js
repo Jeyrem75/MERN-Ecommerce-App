6f68abf0ab76b2b879b89cc4d9540825d5c337a3
Jeyrem75 <ly.jerem@gmail.com>
2023-05-08 17:15:45 +0200

    Fullstack Ecommerce App

---
+++ admin/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
+import { useSelector } from "react-redux";
+
+function App() {
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
+
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route path="/login" element={<Login />} />
+          {admin && (
+            <>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/users" element={<UserList />} />
+                <Route path="/user/:userId" element={<User />} />
+                <Route path="/newUser" element={<NewUser />} />
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/product/:productId" element={<Product />} />
+                <Route path="/newProduct" element={<NewProduct />} />
+            </>
+          )}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
